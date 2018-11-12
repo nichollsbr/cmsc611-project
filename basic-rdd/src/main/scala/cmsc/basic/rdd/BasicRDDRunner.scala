64b1979cd6fb1ff72fecb6cb118298827d6d8885
nichollsbr <nichollsbr@users.noreply.github.com>
2018-11-11 23:20:41 -0500

    Fixed first test

--- basic-rdd/src/main/scala/cmsc/basic/rdd/BasicRDDRunner.scala
+++ basic-rdd/src/main/scala/cmsc/basic/rdd/BasicRDDRunner.scala
@@ -1,6 +1,9 @@
 package cmsc.basic.rdd
 
+import java.io.File
+
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object BasicRDDRunner {
@@ -9,9 +12,26 @@ object BasicRDDRunner {
     implicit val sparkSession: SparkSession = SparkSession.builder().config(new SparkConf()).getOrCreate()
 
     try {
-      println(args)
+      //Work on this part
     } finally {
       sparkSession.stop()
     }
+  }
+
+  //Keep improving
+  def readData()(sparkSession: SparkSession)= {
+    val file = new File("../data/test-gsod")
+    val rdd = sparkSession.sparkContext.textFile(file.getAbsolutePath)
+  }
+}
+
+class BasicRDDRunner {
+  def run(rawData: RDD[String]) : RDD[(String, Long)] = {
+    rawData
+      .mapPartitions(iter => iter.map(s => {
+      val key = s.split("\\s+")
+      (key(0) + " " + key(1), 1l)
+    }))
+      .aggregateByKey(0l)(_+_, _+_)
   }
 }
