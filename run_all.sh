0fc023292a2c672ce1a96f356f88234d51f3aff0
nichollsbr <nichollsbr@users.noreply.github.com>
2018-11-25 21:58:50 -0500

    Fix scripts

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#cd basic-dataframe
-#./run.sh > task_metrics_run_output
-#cd ..
+cd basic-dataframe
+./run.sh > task_metrics_run_output
+cd ..
 
 cd basic-dataset
 ./run.sh > task_metrics_run_output
