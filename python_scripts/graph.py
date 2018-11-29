260a9dd78220e92ebf0599c7ccc82d94b30f7803
bama4 <bama4@umbc.edu>
2018-11-28 19:38:24 -0500

    Updated graph.py script to hae three line graphs of spark thread count analysis

--- python_scripts/graph.py
+++ python_scripts/graph.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[8]:
-
-
-import sys
-# In[9]:
-
-
 import re
+import matplotlib.pyplot as plt
+import json
+import os
 
 INT_TABLE = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
 
@@ -35,39 +29,61 @@ def parse_spark_output_file(header_name, name):
             data_dict[key] = thread_count
     return data_dict
 
+def generate_thread_count_peak_graph(data_directory, file_header, spark_output_path, plot_title, x_label, y_label, **kwargs):
+    files = os.listdir(data_directory)
 
-# In[19]:
+    thread_counts = parse_spark_output_file(file_header, spark_output_path)
+
+    thread_nums = []
+    max_peaks = []
+    for a_file in files:
+        pathed = os.path.join(data_directory, a_file)
+        fp = open(pathed)
+        data_obj = json.load(fp)
+        peak_exec_mems = []
+        for job in data_obj:
+            # Get peak memory values from metric data and thread count of given job
+            peak_exec_mems.append(int(job["peakExecutionMemory"]))
+        thread_nums.append(thread_counts[a_file])
+        max_peaks.append(max(peak_exec_mems))
+        peak_exec_mems = []
+
+    fig = plt.figure()
+    fig.suptitle(plot_title, fontsize=14, fontweight='bold')
+    plt.plot(thread_nums, max_peaks)
+    plt.axis([0,6,0, kwargs.get("max_peak", 10000000)])
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
 
 
-import matplotlib.pyplot as plt
-import json
-import os
+DATAFRAME_DIRECTORY = r"C:\\Users\maria\Documents\Grad School\Fall2018\CMSC611\Projects\spark-stats-dataframe-20181127T232049Z-001\spark-stats-dataframe"
+SPARK_DATAFRAME_OUTPUT_FILE = r"C:\\Users\maria\Documents\Grad School\Fall2018\CMSC611\Projects\spark-stats-dataframe-task_metrics_run_output"
+DATAFRAME_FILE_NAME = "BasicDataframeRunner"
+DATAFRAME_PLOT_TITLE = "Spark DataFrame Metrics"
+DATAFRAME_X_LABEL = "Thread Count"
+DATAFRAME_Y_LABEL = "Peak Memory (Power of 7)"
 
-DATA_DIRECTORY = r"C:\\Users\maria\Documents\Grad School\Fall2018\CMSC611\Projects\spark-stats-dataframe-20181127T232049Z-001\spark-stats-dataframe"
-SPARK_OUTPUT_FILE = r"C:\\Users\maria\Documents\Grad School\Fall2018\CMSC611\Projects\spark-stats-dataframe-task_metrics_run_output"
-FILE_NAME = "BasicDataframeRunner"
-files = os.listdir(DATA_DIRECTORY)
+generate_thread_count_peak_graph(DATAFRAME_DIRECTORY, DATAFRAME_FILE_NAME,
+                                 SPARK_DATAFRAME_OUTPUT_FILE, DATAFRAME_PLOT_TITLE, DATAFRAME_X_LABEL, DATAFRAME_Y_LABEL)
 
-thread_counts = parse_spark_output_file(FILE_NAME, SPARK_OUTPUT_FILE)
-print(thread_counts)
-thread_nums = []
-max_peaks = []
-for a_file in files:
-    pathed = os.path.join(DATA_DIRECTORY, a_file)
-    fp = open(pathed)
-    data_obj = json.load(fp)
-    peak_exec_mems = []
-    for job in data_obj:
-        # Get peak memory values from metric data and thread count of given job
-        peak_exec_mems.append(int(job["peakExecutionMemory"]))
-    thread_nums.append(thread_counts[a_file])
-    max_peaks.append(max(peak_exec_mems))
-    peak_exec_mems = []
 
-ax = plt.plot(thread_nums, max_peaks)
-plt.axis([0, 6, 0, 10000000])
-plt.xlabel("Thread Count")
-plt.ylabel("Peak Memory (Power of 7)")
-plt.show()
+DATASET_DIRECTORY = r"C:\\Users\maria\Documents\Grad School\Fall2018\CMSC611\Projects\spark-stats-datasets-20181127T234407Z-001\spark-stats-datasets"
+SPARK_DATASET_OUTPUT_FILE = r"C:\\Users\maria\Documents\Grad School\Fall2018\CMSC611\Projects\spark-stats-dataset-task_metrics_run_output"
+DATASET_FILE_NAME = "BasicDatasetRunner"
+DATASET_PLOT_TITLE = "Spark Dataset Metrics"
+DATASET_X_LABEL = "Thread Count"
+DATASET_Y_LABEL = "Peak Memory (Power of 7)"
 
-# In[ ]:
+generate_thread_count_peak_graph(DATASET_DIRECTORY, DATASET_FILE_NAME,
+                                 SPARK_DATASET_OUTPUT_FILE, DATASET_PLOT_TITLE, DATASET_X_LABEL, DATASET_Y_LABEL)
+
+RDD_DIRECTORY = r"C:\\Users\maria\Documents\Grad School\Fall2018\CMSC611\Projects\spark-stats-rdd-20181129T000233Z-001\spark-stats-rdd"
+SPARK_RDD_OUTPUT_FILE = r"C:\\Users\maria\Documents\Grad School\Fall2018\CMSC611\Projects\spark-stats-rdd-task_metrics_run_output"
+RDD_FILE_NAME = "BasicRDDRunner"
+RDD_PLOT_TITLE = "Spark RDD Metrics"
+RDD_X_LABEL = "Thread Count"
+RDD_Y_LABEL = "Peak Memory"
+
+generate_thread_count_peak_graph(RDD_DIRECTORY, RDD_FILE_NAME,
+                                 SPARK_RDD_OUTPUT_FILE, RDD_PLOT_TITLE, RDD_X_LABEL, RDD_Y_LABEL, max_peak=6000)
